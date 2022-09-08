92546cdda17e4a6e4cc4f673a5dfc1561e037819
radiosignalz <93599478+radiosignalz@users.noreply.github.com>
2022-09-08 19:49:32 +0300

    Add files via upload

---
+++ binaryNode.php
@@ -0,0 +1,55 @@
+<?php
+class BinaryNode
+{
+    public $value;
+    public $left  = NULL;
+    public $right = NULL;
+
+    public function __construct ($value)
+    {
+        $this->value = $value;
+    }
+}
+
+class BinaryTree
+{
+    protected $root = NULL;
+
+    public function isEmpty ()
+    {
+        return is_null($this->root);
+    }
+
+    public function insert ($value)
+    {
+        $node = new BinaryNode($value);
+        $this->insertNode($node, $this->root);
+    }
+
+    protected function insertNode (BinaryNode $node, &$subtree)
+    {
+        if (is_null($subtree))
+        {
+            $subtree = $node;
+        }
+        else
+        {
+            if ($node->value < $subtree->value)
+            {
+                $this->insertNode($node, $subtree->left);
+            }
+            elseif ($node->value > $subtree->value)
+            {
+                $this->insertNode($node, $subtree->right);
+            }
+        }
+        return $this;
+    }
+}
+
+$tree = new BinaryTree();
+
+$tree->insert(12);
+$tree->insert(32);
+$tree->insert(62);
+var_dump($tree);
